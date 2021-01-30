e6dbc9fd6c4604868a12761828e9691395b2b49b
YashPandey99 <yash.pandey_cs18@gla.ac.in>
2021-01-30 19:36:35 +0530

    Add functtion in arraylist

---
+++ AddarrayList.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * AddarrayList
+ */
+public class AddarrayList {
+
+    public static void main(String[] args) {
+        List <String> obj = new ArrayList<>();
+        obj.add("Yash");
+        obj.add("Lokesh");
+        System.out.println(obj);
+    }
+}
